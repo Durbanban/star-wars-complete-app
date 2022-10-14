7ec2315ca8ea9febc5e4b00b0d5b1b8f0f6a0400
davidgm26 <davidgama260402@gmail.com>
2022-10-14 13:53:01 +0200

    pantalla terminada con toda la base pintada

--- StarWarsCompleteAPP/src/app/components/starships/starships.component.ts
+++ StarWarsCompleteAPP/src/app/components/starships/starships.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { starShip } from 'src/app/interfaces/starship.interface';
+import { StarShipService } from 'src/app/services/star-ship.service';
 
 @Component({
   selector: 'app-starships',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarshipsComponent implements OnInit {
 
-  constructor() { }
+listadoStarShips: starShip[] | undefined;
+numPages = 0;
+
+  constructor(private starShipService: StarShipService) { }
 
   ngOnInit(): void {
+    this.getStarShipPage(1);
   }
 
-}
+  getStarShipPage(page:number){
+    this.starShipService.getStarShip(page).subscribe(resp => {
+      this.listadoStarShips = resp.results
+      this.numPages = Math.ceil(resp.count /10);
+    })
+  }
+  public getPhotoURL(s: starShip): String {
+    return `https://starwars-visualguide.com/assets/img/starships/${
+      s.url.split('/')[5]
+    }.jpg`;
+  }
+
+  contadorPaginas() {
+    return new Array(this.numPages);
+  }
+
+
+  }
+
