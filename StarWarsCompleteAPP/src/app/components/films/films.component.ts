165566f14a913668e48b3727c4760ecbfed5c02e
davidgm26 <davidgama260402@gmail.com>
2022-10-14 14:28:47 +0200

    falta maquetar y agregar HTML

--- StarWarsCompleteAPP/src/app/components/films/films.component.ts
+++ StarWarsCompleteAPP/src/app/components/films/films.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from 'src/app/interfaces/film.interface';
+import { FilmService } from 'src/app/services/film.service';
 
 @Component({
   selector: 'app-films',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmsComponent implements OnInit {
 
-  constructor() { }
+  listadoFilms: Film [] | undefined;
+  numPages = 0;
+  constructor(private filmService: FilmService) { }
 
   ngOnInit(): void {
+    this.getFilmPage(1);
   }
 
+  public getFilmPage(page: number){
+    this.filmService.getFilms(page).subscribe(resp => {
+      this.listadoFilms = resp.results;
+      this.numPages = Math.ceil(resp.count /10);
+
+    })
+  }
+
+  public getPhotoURL(f: Film): String {
+    return `https://starwars-visualguide.com/assets/img/films/${f.url.split('/')[5]}.jpg`;
+  }
+  
 }
