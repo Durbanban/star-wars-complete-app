1ab71d5865b0b3e43b9257b943f6d7a49aa1d1dd
davidgm26 <davidgama260402@gmail.com>
2022-10-15 12:20:52 +0200

    Vehiculos terminados

--- StarWarsCompleteAPP/src/app/components/vehicles/vehicles.component.ts
+++ StarWarsCompleteAPP/src/app/components/vehicles/vehicles.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehicle } from 'src/app/interfaces/vehicle.interface';
+import { VehicleService } from 'src/app/services/vehicle.service';
 
 @Component({
   selector: 'app-vehicles',
   templateUrl: './vehicles.component.html',
-  styleUrls: ['./vehicles.component.css']
+  styleUrls: ['./vehicles.component.css'],
 })
 export class VehiclesComponent implements OnInit {
+  listadoVehiculos: Vehicle[] | undefined;
+  page = 0;
+  numPages = 0;
 
-  constructor() { }
+  constructor(private vehicleService: VehicleService) {}
 
   ngOnInit(): void {
+    this.getVehiclesPage(1);
   }
 
+  contadorPaginas() {
+    return Array(this.numPages);
+  }
+
+  getVehiclesPage(page: number) {
+    this.vehicleService.getVehicles(page).subscribe(resp => {
+      this.listadoVehiculos = resp.results;
+      this.numPages = Math.ceil(resp.count / 10);
+    });
+  }
+
+  public getPhotoURL(v: Vehicle): String {
+    return `https://starwars-visualguide.com/assets/img/vehicles/${
+      v.url.split('/')[5]
+    }.jpg`;
+  }
 }
